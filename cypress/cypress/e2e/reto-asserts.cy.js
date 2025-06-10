33f42fd6b6c0940957d734545601ef7852804365
LUlisesRM <rmilanlu.cue@gmail.com>
2025-06-09 23:58:46 -0600

    Ejercicio Reto Asserts cypress

--- cypress/cypress/e2e/reto-asserts.cy.js
+++ cypress/cypress/e2e/reto-asserts.cy.js
@@ -9,7 +9,52 @@ describe('Reto Asserts', () => {
     cy.title().should('eq', 'Automation QA');
     cy.wait(tiempo);
 
-    //Eliminando ventana
-    
+    cy.get("#sum1").should("be.visible").and("have.class", "form-control").type(10)
+    cy.get("#sum2").should("be.visible").and("have.class", "form-control").type(10)
+
+    cy.contains("[type='button']", "Get Total").click()
+
+    cy.get("#displayvalue").should("be.visible").then((e) => {
+      let r = a + b;
+      cy.log("el valor de r: " + r)
+      cy.log(e.text())
+      let res = e.text()
+
+      if (r == res) {
+        cy.log("Son valores iguales")
+      } else {
+        cy.log("El resultado es incorrecto")
+      }
+
+      if (res > 50) {
+        a = a - 10
+        b = b - 10
+        cy.get("#sum1").invoke("attr", "placeholder").should("contain", "Enter value").then(() => {
+          cy.get("#sum1").clear().type(a)
+          cy.wait(tiempo)
+          cy.get("#sum1").invoke("attr", "style", "color:blue")
+        })
+        cy.wait(tiempo)
+        cy.get("#sum2").should("be.visible").and("have.class", "form-control").clear().type(b)
+        cy.wait(tiempo)
+        cy.get("#sum2").invoke("attr", "style", "color:blue")
+        cy.wait(tiempo)
+        cy.contains("[type='button']", "Get Total").click()
+
+        cy.get("#displayvalue").should("be.visible").then((e) => {
+          cy.get("#displayvalue").invoke("attr", "style", "color:red")
+        })
+      } else {
+        a = a + 5
+        b = b + 5
+        cy.get("#sum1").should("be.visible").and("have.class", "form-control").clear().type(a)
+        cy.wait(tiempo)
+        cy.get("#sum2").should("be.visible").and("have.class", "form-control").clear().type(b)
+        cy.wait(tiempo)
+        cy.contains("[type='button']", "Get Total").click()
+      }
+    })
+
   })
+
 })
