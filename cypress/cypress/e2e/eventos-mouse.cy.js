97f0c800415a0a747d0941ad0e7eafe40fb9e77f
LUlisesRM <rmilanlu.cue@gmail.com>
2025-06-13 00:16:38 -0600

    cy eventos-raton y tablas

--- cypress/cypress/e2e/eventos-mouse.cy.js
+++ cypress/cypress/e2e/eventos-mouse.cy.js
@@ -1,4 +1,36 @@
 /// <reference types="cypress" />
 
 import 'cypress-file-upload';
-require
+require('@4tw/cypress-drag-drop');
+require('cypress-xpath');
+
+describe('Eventos Mouse', () => {
+
+    it('Drag and Drop', () => {
+        let tiempo = 1000;
+        cy.visit('https://lulisesrm.github.io/automation-qa/')
+        cy.title().should('eq', 'Automation QA');
+        cy.wait(tiempo);
+
+        cy.get("#column-a").drag("column-b", {force:true})
+    });
+
+    it('Mouse Over', () => {
+        let tiempo = 1000;
+        cy.visit('https://lulisesrm.github.io/automation-qa/')
+        cy.title().should('eq', 'Automation QA');
+        cy.wait(tiempo);
+
+        cy.get('.menu-bar > :nth-child(1) > :nth-child(1)').trigger("mouseover");
+        cy.wait(tiempo);
+    });
+
+    it.only('Slider', () => {
+        let tiempo = 1000;
+        cy.visit('https://lulisesrm.github.io/automation-qa/site-resources/PDP/pdp.html')
+       // cy.title().should('eq', 'Automation QA');
+        cy.wait(tiempo);
+
+        cy.get('.product-price').invoke('attr', 'style', 'color: red;');
+    });
+})
---
+++ cypress/cypress/e2e/tablas.cy.js
@@ -0,0 +1,81 @@
+/// <reference types='cypress' />
+
+import 'cypress-file-upload';
+
+require('@4tw/cypress-drag-drop');
+require('cypress-xpath');
+require('cypress-plugin-tab');
+
+describe('Elementos de una tabla', ()=>{
+
+    it('CHILDREN', () => {
+        let tiempo = 1000;
+        cy.visit('https://lulisesrm.github.io/automation-qa/index.html');
+        cy.title().should('eq', 'Automation QA');
+        cy.wait(tiempo);
+
+        cy.get('.btn-group').children('button').should('contain','Green').click({force: true});
+    });
+
+    
+    it('Seleccionar por medio de EQ', () => {
+        let tiempo = 1000;
+        cy.visit('https://lulisesrm.github.io/automation-qa/index.html');
+        cy.title().should('eq', 'Automation QA');
+        cy.wait(tiempo);
+
+        cy.get('[type="button"]').eq(2).should('contain','Red').click({force: true});
+    });
+
+    
+    it('Seleccionar por medio de Filter', () => {
+        let tiempo = 1000;
+        cy.visit('https://lulisesrm.github.io/automation-qa/index.html');
+        cy.title().should('eq', 'Automation QA');
+        cy.wait(tiempo);
+
+        cy.get('[type="button"]').filter('.btn-warning');
+    });
+
+    it('Seleccionar por medio de Find', () => {
+        let tiempo = 1000;
+        cy.visit('https://lulisesrm.github.io/automation-qa/index.html');
+        cy.title().should('eq', 'Automation QA');
+        cy.wait(tiempo);
+
+        cy.get('.btn-group').find('[type="button"]').should('have.length', 3);
+    });
+
+    it('Reto de las tablas con for y Assets', () => {
+        let tiempo = 1000;
+        cy.visit('https://lulisesrm.github.io/automation-qa/index.html');
+        cy.title().should('eq', 'Automation QA');
+        cy.wait(tiempo);
+
+        for(let x = 0; x <= 4; x++){
+            let nombreBoton = "";
+
+            if (x == 1) {
+                nombreBoton = "Green";  
+                
+            } else if (x == 2) {
+                nombreBoton = "Red";  
+                
+            } else if (x == 3) {
+                nombreBoton = "Blue";  
+                
+            } else if (x == 4) {
+                nombreBoton = "All";   
+            }
+
+            cy.get('[type="button"]').eq(x).should('contain', nombreBoton).click({force: true});
+            cy.wait(tiempo);
+            cy.get('[type="checkbox"]').check({force: true});
+        }
+    });
+
+    
+
+
+
+});
